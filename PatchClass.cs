072c744bd79086c197c71a5475e8861b8afe8e0f
ETIQUE Kevin <ketique04@gmail.com>
2024-09-08 21:29:18 +0200

    Add files via upload

---
+++ PatchClass.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System.IO;
+using BepInEx;
+using UnityEngine;
+using BepInEx.Logging;
+using Logger = BepInEx.Logging.Logger;
+
+namespace caca
+{
+    [HarmonyPatch(typeof(HoarderBugAI))]
+    public class PatchClass
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void hoarderBugPatch(HoarderBugAI __instance ,ref AudioClip[] ___chitterSFX )
+        {
+            AudioClip[] newChitterSfx = Plugin.newSFX;
+            
+            if (__instance == null)
+            {
+                Logger.CreateLogSource("caca").LogError("Instance is null!");
+                return;
+            }
+            
+            
+            if (newChitterSfx == null || newChitterSfx.Length == 0)
+            {
+                Logger.CreateLogSource("caca").LogError("Failed to load audio assets! 2");
+                return;
+            }
+            
+            
+            if (___chitterSFX == null)
+            {
+                Logger.CreateLogSource("caca").LogError("___chitterSFX is null!");
+                return;
+            }
+            
+            ___chitterSFX = newChitterSfx;
+        }
+    } 
+}
---
+++ Plugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+
+
+namespace caca
+{
+    [BepInPlugin("com.sau6son.cacaPlugin", "caca hehe", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony _harmony;
+        
+        private static Plugin Instance;
+        
+        internal static AudioClip[] newSFX;
+
+        void Awake()
+        {
+            if ((Object)(object)Instance == (Object)null)
+            {
+                Instance = this;
+            }
+            
+            // Initialiser Harmony et patcher les méthodes
+            _harmony = new Harmony("com.sau6son.cacaPlugin");
+            _harmony.PatchAll();
+            
+            Logger.LogInfo("sau6son.caca is loading.");
+            
+            string location = ((BaseUnityPlugin)Instance).Info.Location;
+            string text = "caca.dll";
+            string text2 = location.TrimEnd(text.ToCharArray());
+            string text3 = text2 + "caca-bundle";
+            AssetBundle val = AssetBundle.LoadFromFile(text3);
+            if ((Object)(object)val == (Object)null)
+            { 
+                Logger.LogError((object)"Failed to load audio assets!"); 
+                return;
+            }
+            
+            newSFX = val.LoadAssetWithSubAssets<AudioClip>("assets/caca.mp3");
+            if (newSFX == null || newSFX.Length == 0)
+            {
+                Logger.LogError((object)"Failed to load audio assets! 1");
+                return;
+            }
+            else
+            {
+                Logger.LogInfo((object)"Loaded audio assets " + newSFX[0].name);
+            }
+            _harmony.PatchAll(typeof(PatchClass));
+            Logger.LogInfo((object) "caca plugin loaded !");
+
+        }
+    }
+}
